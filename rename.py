d2690ae25b9817cef29623e880ecd80ad0a4e746
Samuel <201501502@rajalakshmi.edu.in>
2022-12-14 16:10:18 +0530

    Uploaded the Jan Files after rename

---
+++ rename.py
@@ -0,0 +1,15 @@
+import os
+
+# Set the path to the folder containing the files you want to rename
+path = "January"
+
+# Get a list of the files in the folder
+files = os.listdir(path)
+i=1
+# Iterate over the list of files
+for file in files:
+  # Compute the new file name
+  new_name = file.replace(file, "1-"+str(i)+".pdf")
+  i+=1
+  # Use the os.rename() method to rename the file
+  os.rename(os.path.join(path, file), os.path.join(path, new_name))
